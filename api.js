f9342096b302b6d009816da3e6426709fab706ed
vanessa <vane.busato9@gmail.com>
2026-02-17 20:50:30 +0100

    sistemato app.use

--- api.js
+++ api.js
@@ -4,6 +4,11 @@ const postsRouter = require("./routers/posts")
 const app = express()
 const port = 3000
 
+app.use(express.json());
 app.use(express.static('public'));
 
-app.use("/posts, postsRouter")
+app.use("/posts", postsRouter)
+
+app.listen(port, () => {
+    console.log(`Server avviato su http://localhost:${port}`);
+});
